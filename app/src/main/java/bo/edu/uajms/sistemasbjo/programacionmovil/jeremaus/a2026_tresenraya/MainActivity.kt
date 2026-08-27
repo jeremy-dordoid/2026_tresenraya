e23ea14dee6ff1b561edf6fad2ecac8c56a2897b
jeremy-dordoid <jeremiasgareca4@gmail.com>
2026-08-27 18:25:39 -0300

    Initial commit

---
+++ app/src/main/java/bo/edu/uajms/sistemasbjo/programacionmovil/jeremaus/a2026_tresenraya/MainActivity.kt
@@ -0,0 +1,12 @@
+package bo.edu.uajms.sistemasbjo.programacionmovil.jeremaus.a2026_tresenraya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
+        setContentView(R.layout.activity_main)
+    }
+}
